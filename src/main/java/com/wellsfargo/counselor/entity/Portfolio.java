f7116f0a7182356d3dc79482aff870a112317732
RijulNambiar <rijulnambiar@gmail.com>
2026-06-07 01:12:39 +0530

    Implemented Wells Fargo investment management data model

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,47 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private long clientId;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(long clientId, LocalDate creationDate) {
+        this.clientId = clientId;
+        this.creationDate = creationDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+}
